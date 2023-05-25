03b9945036c06aefd3c937ad977242641601efd7
Mikosztyla <mikosztyla@gmai.com>
2023-05-25 10:14:07 +0200

    asd

--- asd/2sem/offline5/zad6.py
+++
@@ -1,8 +0,0 @@
-from zad6testy import runtests
-
-def binworker( M ):
-    # tu prosze wpisac wlasna implementacje
-    return 0
-
-# zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( binworker, all_tests = False )
